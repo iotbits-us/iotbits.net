47db0044083b07b4b04899460d49f5e367c78f96
Evert Arias <evert.arias@hotmail.com>
2020-03-26 01:49:25 -0400

    Update Contact Form

---
+++ src/pages/thanks.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../layouts/index';
+
+const Thanks = () => (
+  <Layout>
+    <p>Your info has been submitted!</p>
+  </Layout>
+);
+
+export default Thanks;
