b719dfa8cff58bac9908685db3aff2a031deb56a
Evert Arias <evert.arias@hotmail.com>
2020-07-27 15:20:17 -0400

    Update

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -38,7 +38,7 @@ class Form extends React.Component {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
         'form-name': form.getAttribute('name'),
-        ...this.state
+        name: 'Evert'
       })
     })
       .then(res => {
